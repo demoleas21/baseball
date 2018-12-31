8cdbcd1b25b4b003500b0b96046297df31276b6d
demoleas21 <andrew.demoleas@gmail.com>
2018-12-30 20:10:16 -0500

    Changing website used to fetch data

--- src/configs.py
+++ src/configs.py
@@ -21,3 +21,54 @@ SAC = 'Sacrifices'
 PA = 'Plate Appearances'
 
 STAT_CATEGORIES = [GP, AB, R, H, B1, B2, B3, HR, TB, BB, RBI, HBP, K, SB, AVG, OBP, SLG, OPS, SF, SAC, PA]
+
+NJABL_FALL_2018 = 'https://www.newjerseyabl.com/stats/team_instance/3824651?subseason=536707&tab=team_instance_player_stats&tool=3261383'
+NJABL_PLAYER_PAGE = 'https://www.newjerseyabl.com/roster_players/{playerId}'
+
+ANDREW = {
+    'name': 'Andrew Demoleas',
+    'pageId': '26469546'
+}
+BLOSS = {
+    'name': 'Ryan Godfrey',
+    'pageId': '26469533'
+}
+DRE = {
+
+}
+BEN = {
+
+}
+BROC = {
+
+}
+CHRISTIAN = {
+
+}
+FRANK = {
+
+}
+BRENT = {
+
+}
+TYLER = {
+
+}
+AIRTIGHT = {
+
+}
+MATEUSZ = {
+
+}
+LUIGI = {
+
+}
+JOHN = {
+
+}
+STEVE = {
+
+}
+ROB = {
+
+}
--- src/lineup.py
+++ src/lineup.py
@@ -4,19 +4,39 @@ from bs4 import BeautifulSoup
 import configs
 
 
-def soupie():
+def tempGetPlayerStats(player):
     playerDict = {}
-    njabl = 'https://www.newjerseyabl.com/stats/team_instance/3824651?subseason=536707&tab=team_instance_player_stats&tool=3261383'
-    statsPage = urllib2.urlopen(njabl)
+    statsPage = urllib2.urlopen(configs.NJABL_FALL_2018)
     soup = BeautifulSoup(statsPage, 'lxml')
-    tbody = soup.tbody
-    bloss = tbody.td.next.next.next.text.split('\n')[1]
-    playerStats = [float(text.text) for text in tbody.tr.find_all('td')[2:]]
-    playerDict[bloss] = zip(configs.STAT_CATEGORIES, playerStats)
+    playerStatRows = soup.tbody.find_all('tr')
+    for i, _ in enumerate(playerStatRows):
+        if player == playerStatRows[i].text.split('\n')[3]:
+            playerStats = playerStatRows[i].text.split('\n')[5:26]
+            break
+    playerDict[player] = {}
+    for i, _ in enumerate(playerStats):
+        playerDict[player][configs.STAT_CATEGORIES[i]] = float(playerStats[i])
+    pprint(playerDict)
+
+
+def getPlayerStats(player):
+    playerDict = {}
+    statsPage = urllib2.urlopen(configs.NJABL_FALL_2018)
+    soup = BeautifulSoup(statsPage, 'lxml')
+    playerStatRows = soup.tbody.find_all('tr')
+    for i, _ in enumerate(playerStatRows):
+        if player == playerStatRows[i].text.split('\n')[3]:
+            playerStats = playerStatRows[i].text.split('\n')[5:26]
+            break
+    playerDict[player] = {}
+    for i, _ in enumerate(playerStats):
+        playerDict[player][configs.STAT_CATEGORIES[i]] = float(playerStats[i])
     pprint(playerDict)
 
 
 def main():
-    soupie()
+    tempGetPlayerStats(configs.BLOSS)
+    tempGetPlayerStats(configs.ANDREW)
+
 
 main()
