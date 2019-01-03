1a3fc3729551b81ac64ca3310684249549c9c468
demoleas21 <andrew.demoleas@gmail.com>
2019-01-03 18:12:04 -0500

    Changed models for player and used new link for connection

--- src/lineup.py
+++ src/lineup.py
@@ -2,11 +2,15 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 import configs
 from selenium import webdriver
+from time import sleep
 
 
 def getSoup(website):
     driver = webdriver.Chrome()
     driver.get(website)
+    careerStatsButton = driver.find_element_by_link_text('Career Stats')
+    careerStatsButton.click()
+    sleep(3)
     html = driver.page_source
     return BeautifulSoup(html, "html.parser")
 
@@ -16,15 +20,14 @@ def getPlayerStats(player):
     website = configs.NJABL_PLAYER_PAGE.format(playerId=player.pageId)
     soup = getSoup(website)
     # TODO: use driver to click buttons on website to see other tabs
-    playerStatRows = soup.find_all('tr')
-    for i, _ in enumerate(playerStatRows):
-        if player.name == playerStatRows[i].text.split('\n')[3]:
-            playerStats = playerStatRows[i].text.split('\n')[5:26]
-            break
+    regularSeasonFall2018 = soup.find_all('tr')[8].text.split('\n')[3:24]
+    regularSeasonSpring2018 = soup.find_all('tr')[9].text.split('\n')[3:24]
+    careerTotals = soup.find_all('tr')[10].text.split('\n')[3:24]
     playerDict[player.name] = {}
-    for i, _ in enumerate(playerStats):
-        playerDict[player.name][configs.STAT_CATEGORIES[i]] = float(playerStats[i])
-    pprint(playerDict)
+    for i, _ in enumerate(regularSeasonFall2018):
+        playerDict[player.name][configs.STAT_CATEGORIES[i]] = float(regularSeasonFall2018[i])
+    player.setStats('regularSeasonFall2018', playerDict[player.name])
+    pprint(player.regularSeasonFall2018)
 
 
 def main():
--- src/models.py
+++ src/models.py
@@ -3,7 +3,7 @@ class Player(object):
     def __init__(self, name, pageId):
         self.name = name
         self.pageId = pageId
-        self.stats = None
+        self.regularSeasonFall2018 = None
 
-    def setStats(self, data):
-        self.stats = data
+    def setStats(self, category, data):
+        setattr(self, category, data)
